6ac553705cdffc575d5c7de90add7a9895dea740
00Unreal00 <ermakovnk@gmail.com>
2024-10-28 00:47:56 +0300

    part3

--- part3.py
+++ part3.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plot
-
+from collections import Counter
 data = {'A': [], 'B': [], 'C': []}
-with open('data/1361_e_artem.csv') as f:
+with open('4731_my.csv') as f:
     keys = list(data.keys())
     change_pos = 0
     count = 0
@@ -36,6 +36,9 @@ def fischer_tree_class(class1, class2, class3):
 
 u = fischer_tree_class(data.get("A"), data.get("B"), data.get("C"))
 print(f"Признак {u[0]} {u[1]} Признак {u[2]} {u[3]}")
+
+
+#графики
 xa = data.get("A")[:, u[0] - 1]
 ya = data.get("A")[:, u[2] - 1]
 plot.scatter(xa, ya, c='red')
@@ -61,25 +64,40 @@ xaa = np.mean(data.get("A"), axis=0)
 xaa = np.array([xaa[u[0] - 1], xaa[u[2] - 1]])
 xbb = np.mean(data.get("B"), axis=0)
 xbb = np.array([xbb[u[0] - 1], xbb[u[2] - 1]])
-# xaa = np.array([1.5, 1.5])
-# xbb = np.array([3.5, 3.5])
 xasb = (xaa - xbb).reshape(1, 2)  # матрица 1 строка 2 столбца
 xaab = (xaa + xbb).reshape(2, 1)  # матрица 2 строки 1 столбец
-qq = np.dot(xasb, xaab).item()  # получение простого числа из массива
-x2 = [s for s in range(-100, 100, 5)]
-x1 = [((-(s * xasb[0, 1]) + 0.5 * qq) / xasb[0, 0]) for s in x2]
-plot.plot(x1, x2)
-
+n = np.dot(xasb, xaab).item()  # получение простого числа из массива
+x2 = [s for s in range(-100, 200, 5)]
+x1 = [((-(s * xasb[0, 1]) + 0.5 * n) / xasb[0, 0]) for s in x2]
+plot.plot(x1, x2, c='purple')
+'''created by 00Unreal00
+'''
 # вторая прямая
-xab = np.mean(data.get("A"), axis=0)+np.mean(data.get("B"), axis=0)
+xab = np.sum(data.get("A") + data.get("B"), axis=0) / (len(data.get("A")) * 2)
 xab = np.array([xab[u[0] - 1], xab[u[2] - 1]])
-xc = np.mean(data.get("C"), axis=0)
-xc = np.array([xc[u[0] - 1], xc[u[2] - 1]])
-xabsc = (xab - xc).reshape(1, 2)  # матрица 1 строка 2 столбца
-xabac = (xab + xc).reshape(2, 1)  # матрица 2 строки 1 столбец
-q = np.dot(xabsc, xabac).item()  # получение простого числа из массива
-x22 = [s for s in range(-100, 100, 5)]
-x11 = [((-(s * xasb[0, 1]) + 0.5 * q) / xasb[0, 0]) for s in x22]
-plot.plot(x11, x22)
+xc2 = np.mean(data.get("C"), axis=0)
+xc2 = np.array([xc2[u[0] - 1], xc2[u[2] - 1]])
+xabsc = (xab - xc2).reshape(1, 2)  # матрица 1 строка 2 столбца
+xabac = (xab + xc2).reshape(2, 1)  # матрица 2 строки 1 столбец
+n = np.dot(xabsc, xabac).item()  # получение простого числа из массива
+x11 = [((-(s * xabsc[0, 1]) + 0.5 * n) / xabsc[0, 0]) for s in x2]
+plot.plot(x11, x2, c='brown')
 plot.show()
 
+
+def distance(pnt1, pnt2):
+    d = np.sqrt(np.sum((pnt1 - pnt2) ** 2))
+    return d
+
+
+points = np.vstack([np.column_stack([xa, ya]), np.column_stack([xb, yb]), np.column_stack([xc, yc])])
+
+
+def knn(point, k=5):  # k nearest neighbors - к ближайших соседей
+    cls = np.array(['A'] * len(data.get("A")) + ['B']*len(data.get("B")) + ['C']*len(data.get("C")))
+    dist = [[distance(point, p), cl] for p, cl in zip(points, cls)]
+    dist = sorted(dist, key=lambda x: x[0])[1:k+1]
+    print(dist)
+    dist = [i[1] for i in dist]
+    print(Counter(dist).most_common()[0][0])
+knn(np.array([15,30]))
